b7dde62b8dabbe9e0ed16e1031822bc06e95dfab
Matthew Amorelli <77508333+Amorelli-Matthew@users.noreply.github.com>
2025-11-16 20:43:16 -0800

    Update Button.h

--- Button.h
+++ Button.h
@@ -1,34 +1,33 @@
 #ifndef BUTTON_H
 #define BUTTON_H
-//for handling buttons and their logic
-
 #include <Arduino.h>
 #include "Serial.h"
 
+//use the pins 2 and 36 for the startandstop button and the reset button respectively
+
+
+#define StartStopButtonPin 2
+#define STARTSTOP_DEBOUNCE_US 3000000 //17 sec debounce time
+#define RESET_DEBOUNCE_US 50000UL  // 50 ms debounce time
 
 ///we declare the variables first, Then init them in the resective cpp file
 
-extern unsigned long previousTime;   // when the raw reading last flipped
+
+extern unsigned long previousTime; 
 extern unsigned long currentTime;
 
-//vars for the last reading
+//vars for the last reading of startstopbutton
 extern int lastReading;
 extern int stableState;
 extern int reading;
 
+//bool to keep track of when the button was pressed
 extern volatile bool StartStopButtonEvent;    
-//extern volatile bool ProgramStatus; 
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#define StartStopButtonPin 2
-#define STARTSTOP_DEBOUNCE_US 3000000 //17 sec
-
-#define RESET_DEBOUNCE_US 50000UL  // 50 ms debounce time
-#define PC1_BIT 1             // bit 1 = pin 36
-         
+        
 
 extern volatile unsigned char *my_DDRE;  // DDR for Port E for starting stopping program button, this is for pin 2
 extern volatile unsigned char *my_PORTE;  // PORT for Port E. 
@@ -43,9 +42,6 @@ void StartStopISR();
 
 //init reset button
 void ResetButtonInit();
-
-
-
 
 #ifdef __cplusplus
 }
